32e1f0705a6ec5565c1197b4c824b710f9e57942
winiyaz <winiyaz@proton.me>
2024-07-27 03:07:09 -0700

    💕💕 - Padle up and down

--- ver1/main.py
+++ ver1/main.py
@@ -8,12 +8,28 @@ scr.bgcolor("#020617")
 scr.setup(1000, 800)
 scr.title("SitOnFace")
 
+# Setting up the padle
 padle = Turtle()
 padle.shape("square")
 padle.color("#22c55e")
-padle.shapesize(stretch_wid=5,stretch_len=1)
+padle.shapesize(stretch_wid=5, stretch_len=1)
+padle.penup()
+padle.goto(450, 0)
 
 
+def go_up():
+	new_y = padle.ycor() + 20
+	padle.goto(padle.xcor(), new_y)
+
+
+def go_do():
+	new_y = padle.ycor() - 20
+	padle.goto(padle.xcor(), new_y)
+
+
+scr.listen()
+scr.onkey(go_up, "Up")
+scr.onkey(go_do, "Down")
 
 # -- Exit on Click
 scr.exitonclick()
