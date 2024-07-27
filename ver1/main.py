fd5b02a7b2dff1109d874d7bb706c529a19892c0
winiyaz <winiyaz@proton.me>
2024-07-27 03:02:55 -0700

    💕💕 - Settinup Paddle

--- ver1/main.py
+++ ver1/main.py
@@ -1,6 +1,6 @@
 # Pong Game
 
-from turtle import Screen
+from turtle import Screen, Turtle
 
 # Screen setup
 scr = Screen()
@@ -8,8 +8,10 @@ scr.bgcolor("#020617")
 scr.setup(1000, 800)
 scr.title("SitOnFace")
 
-
-
+padle = Turtle()
+padle.shape("square")
+padle.color("#22c55e")
+padle.shapesize(stretch_wid=5,stretch_len=1)
 
 
 
