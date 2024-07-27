79004e0f4fb1f3ae0b04555256c000cc98b8686a
winiyaz <winiyaz@proton.me>
2024-07-27 02:54:26 -0700

    💕💕 - Screen setup

--- ver1/main.py
+++ ver1/main.py
@@ -1,1 +1,17 @@
-# Main work
+# Pong Game
+
+from turtle import Screen
+
+# Screen setup
+scr = Screen()
+scr.bgcolor("#020617")
+scr.setup(1000, 800)
+scr.title("SitOnFace")
+
+
+
+
+
+
+# -- Exit on Click
+scr.exitonclick()
